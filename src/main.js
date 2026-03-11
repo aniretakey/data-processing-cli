1df1539da590f83bc405bfc877e729a23a1d1ec7
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-11 20:00:07 +0300

    add main js cli prompt

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log('start!')
+import readline from "readline";
+
+const app = () => {
+  const readLine = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "> ",
+  });
+};
+
+app();
