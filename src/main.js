919b44b4ff9bfdb56e9db91e1f16c391169bced9
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-11 20:18:00 +0300

    add base app commands with prompt

--- src/main.js
+++ src/main.js
@@ -6,6 +6,20 @@ const app = () => {
     output: process.stdout,
     prompt: "> ",
   });
+
+  console.log("Welcome to Data Processing CLI!");
+  readLine.prompt();
+
+  readLine.on("line", (line) => {
+    const lineCommandWithoutWhitespaces = line.trim();
+    console.log("input command:", lineCommandWithoutWhitespaces);
+    readLine.prompt();
+  });
+
+  readLine.on("SIGINT", () => {
+    console.log("Thank you for using Data Processing CLI!");
+    process.exit();
+  });
 };
 
 app();
