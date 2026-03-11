00f296ef07abba8d0ad16537cd6ebe859674fab1
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-11 21:27:33 +0300

    update move up directory, hide curr path message for root path

--- src/modules/navigation/movesUpOneDirectory.js
+++ src/modules/navigation/movesUpOneDirectory.js
@@ -1,13 +1,15 @@
 import { resolve } from "node:path";
+import { COMMAND_RESULT } from "../../const/const.js";
 
 export const movesUpOneDirectory = () => {
   const currentDir = process.cwd();
   const upperDir = resolve(currentDir, "..");
 
   if (currentDir === upperDir) {
-    console.log(`Already in root directory!`);
-    return;
+    return COMMAND_RESULT.NOTHING;
   }
 
   process.chdir(upperDir);
+
+  return COMMAND_RESULT.SUCCESS;
 };
