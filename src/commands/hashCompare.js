0ba0ffc4ab840b7d914ff0065a9b398923449dfd
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-15 12:25:02 +0300

    add supported algorithms check

--- src/commands/hashCompare.js
+++ src/commands/hashCompare.js
@@ -5,8 +5,15 @@ import { COMMAND_RESULT } from "../utils/const.js";
 // cd E:\dev\Rolling Scopes School\RSS-Node\data-processing-cli\src
 // hash-compare --input text.txt --hash text.txt.sha256  --algorithm md5
 
+const supportedAlgorithms = ["sha256", "md5", "sha512"];
+
 export const hashCompare = async (args) => {
   const { file, hashFile, algorithm } = parseHashCompareArgs(args);
+
+  if (!supportedAlgorithms.includes(algorithm)) {
+    console.log(`Selected algorithm ${algorithm} doesn't support!`);
+    return COMMAND_RESULT.ERROR;
+  }
 
   console.log("file", file);
   console.log("hashFile", hashFile);
