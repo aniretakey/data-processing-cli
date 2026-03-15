3da569e9d298df41df8102bfaf69ec959235b722
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-15 11:50:54 +0300

    update hash

--- src/commands/hash.js
+++ src/commands/hash.js
@@ -1,7 +1,31 @@
 import { parseHashArgs } from "../utils/argParser.js";
 import { COMMAND_RESULT } from "../utils/const.js";
+import { createHash } from "crypto";
+import { Transform } from "stream";
+import { pipeline } from "stream/promises";
+import { createReadStream } from "fs";
+import { resolveAndCheckFileExists } from "../utils/pathResolver.js";
 
 const supportedAlgorithms = ["sha256", "md5", "sha512"];
+
+// TODO: for test: hash --input text.txt --algorithm md5 --save
+
+const createHashTransform = (algorithm) => {
+  const hash = createHash(algorithm);
+
+  return new Transform({
+    transform(chunk, encoding, callback) {
+      hash.update(chunk);
+      callback();
+    },
+
+    flush(callback) {
+      const digest = hash.digest("hex");
+      console.log(`${algorithm}: ${digest}`);
+      callback(null, digest);
+    },
+  });
+};
 
 export const hash = async (args) => {
   const { file, algorithm, save } = parseHashArgs(args);
@@ -14,6 +38,10 @@ export const hash = async (args) => {
     console.log(`Selected algorithm ${algorithm} doesn't support!`);
     return COMMAND_RESULT.ERROR;
   }
+
+  const filePath = resolveAndCheckFileExists(file);
+
+  await pipeline(createReadStream(filePath), createHashTransform(algorithm));
 
   return COMMAND_RESULT.NOTHING;
 };
