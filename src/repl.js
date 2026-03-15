650ba9ecc9a6b204b331a49cecce9540d847ca70
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-15 09:23:19 +0300

    add repl file (according to task description)

---
+++ src/repl.js
@@ -0,0 +1,43 @@
+import { handleExit } from "./utils/handleExit.js";
+import { initReadline } from "./utils/initReadline.js";
+import { changeToSpecifiedDirectory, listContentForCurrentDirectory, movesUpOneDirectory } from "./navigation.js";
+import { parseInput } from "./utils/argParser.js";
+
+const cmdActions = {
+  ".exit": handleExit,
+  up: movesUpOneDirectory,
+  ls: listContentForCurrentDirectory,
+};
+
+export const initRepl = () => {
+  const { readLine, startApp, processStatus, handleIncorrectCommand } = initReadline();
+
+  startApp();
+
+  readLine.on("line", (line) => {
+    const { cmd, args } = parseInput(line);
+
+    if (!cmd) {
+      return;
+    }
+
+    handleIncorrectCommand(cmd);
+
+    let commandStatus;
+
+    if (cmd in cmdActions) {
+      commandStatus = cmdActions[cmd]();
+    }
+
+    if (cmd === "cd") {
+      const newPath = args[0];
+      commandStatus = changeToSpecifiedDirectory(newPath);
+    }
+
+    processStatus(commandStatus);
+  });
+
+  readLine.on("SIGINT", () => {
+    handleExit();
+  });
+};
