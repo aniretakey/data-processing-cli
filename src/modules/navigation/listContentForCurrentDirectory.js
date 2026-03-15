f3805982e9c1c7969fcb0e24fc07565b9129a177
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-15 09:03:35 +0300

    update function ls

--- src/modules/navigation/listContentForCurrentDirectory.js
+++ src/modules/navigation/listContentForCurrentDirectory.js
@@ -1,8 +1,32 @@
-import { readdirSync } from "fs";
+import { readdirSync, statSync } from "fs";
+import { COMMAND_RESULT } from "../../const/const.js";
+import { resolve } from "node:path";
+
+const logItem = (item) => {
+  console.log(item);
+};
 
 export const listContentForCurrentDirectory = () => {
   const currentDir = process.cwd();
 
   const list = readdirSync(currentDir);
-  console.log("list:", list);
+
+  const files = [];
+  const folders = [];
+
+  list.forEach((item) => {
+    const pathToElem = resolve(currentDir, item);
+    const stat = statSync(pathToElem);
+
+    if (stat.isDirectory()) {
+      folders.push(`${item} [folder]`);
+    } else {
+      files.push(`${item} [file]`);
+    }
+  });
+
+  folders.sort((a, b) => a.localeCompare(b)).forEach(logItem);
+  files.sort((a, b) => a.localeCompare(b)).forEach(logItem);
+
+  return COMMAND_RESULT.NOTHING;
 };
