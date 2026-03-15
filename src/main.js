3f54eb5f26a392fadc13a4d4f331ff6bc157fd7e
Ekaterina Kornishina <emkornishina@gmail.com>
2026-03-15 07:43:32 +0300

    update main

--- src/main.js
+++ src/main.js
@@ -2,11 +2,27 @@ import { handleExit } from "./modules/handleExit.js";
 import { initReadline } from "./modules/initReadline.js";
 import { movesUpOneDirectory } from "./modules/navigation/movesUpOneDirectory.js";
 import { COMMAND_RESULT } from "./const/const.js";
+import { changeToSpecifiedDirectory } from "./modules/navigation/changeToSpecifiedDirectory.js";
 
 const cmdActions = {
   ".exit": handleExit,
   up: movesUpOneDirectory,
 };
+
+const allowedCommandsList = [
+  ".exit",
+  "up",
+  "cd",
+  // "ls",
+  // "csv-to-json",
+  // "json-to-csv",
+  // "count",
+  // "hash",
+  // "encrypt",
+  // "decrypt",
+  // "log-stats",
+  // "hash-compare",
+];
 
 const app = () => {
   const { readLine, startApp, addPrompt, handleSuccessCommand, handleIncorrectCommand, handleFailedCommand } =
@@ -17,22 +33,29 @@ const app = () => {
   readLine.on("line", (line) => {
     const lineCommandWithoutWhitespaces = line.trim();
 
-    if (lineCommandWithoutWhitespaces in cmdActions) {
-      const commandStatus = cmdActions[lineCommandWithoutWhitespaces]();
-
-      if (commandStatus === COMMAND_RESULT.NOTHING) {
-        addPrompt();
-      }
-
-      if (commandStatus === COMMAND_RESULT.SUCCESS) {
-        handleSuccessCommand();
-      }
-
-      if (commandStatus === COMMAND_RESULT.ERROR) {
-        handleFailedCommand();
-      }
-    } else {
+    if (!allowedCommandsList.includes(lineCommandWithoutWhitespaces)) {
       handleIncorrectCommand();
+    }
+
+    let commandStatus;
+
+    if (lineCommandWithoutWhitespaces in cmdActions) {
+      commandStatus = cmdActions[lineCommandWithoutWhitespaces]();
+    }
+    if (lineCommandWithoutWhitespaces === "cd") {
+      changeToSpecifiedDirectory("test");
+    }
+
+    if (commandStatus === COMMAND_RESULT.NOTHING) {
+      addPrompt();
+    }
+
+    if (commandStatus === COMMAND_RESULT.SUCCESS) {
+      handleSuccessCommand();
+    }
+
+    if (commandStatus === COMMAND_RESULT.ERROR) {
+      handleFailedCommand();
     }
   });
 
